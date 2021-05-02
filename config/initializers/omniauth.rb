1fc6be48a7c2234a0b685114a0f446133405bb28
hide <razyetts@hotmail.co.jp>
2021-05-02 17:48:32 +0900

    動作確認用にtwitterも追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   require 'omniauth/strategies/line' # なかったらエラー出た
   provider :line, ENV['channel_id'], ENV['channel_secret']
-  # provider :twitter, Rails.application.credentials.twitter[:api_key], Rails.application.credentials.twitter[:api_secret]
+
+  provider :twitter, ENV['api_key'], ENV['api_secret']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/test', to: 'test#index'
-  post '/auth/line/callback', to: 'sessions#create'
+  post '/auth/:provider/callback', to: 'sessions#create'
 end
