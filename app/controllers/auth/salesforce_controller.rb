edcc8f0da1121e4d3187559bedb65d8f6e40d64a
hide <razyetts@hotmail.co.jp>
2022-08-06 21:51:36 +0900

    salesforceアクセストークンを試しに使ってみたかったので

--- app/controllers/auth/salesforce_controller.rb
+++ app/controllers/auth/salesforce_controller.rb
@@ -15,5 +15,10 @@ class Auth::SalesforceController < ApplicationController
   def callback
     code = params[:code]
     salesforce_oauth = SalesforceOauth.retrieve_access_token(code)
+
+    salesforce_user_info = SalesforceUserInfo.retrieve(salesforce_oauth.access_token)
+    given_name = salesforce_user_info.given_name
+
+    render plain: "given_name: #{given_name}"
   end
 end
